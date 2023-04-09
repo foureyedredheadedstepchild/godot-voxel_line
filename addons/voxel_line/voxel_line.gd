54d69621c869229adf37ee2933749de44b5732eb
Nala Nedwob <nedwob@live.com>
2023-04-09 11:56:07 -0300

    Add files via upload

--- addons/voxel_line/voxel_line.gd
+++ addons/voxel_line/voxel_line.gd
@@ -1,13 +1,17 @@
 @tool
-# @icon("voxel_line.png")
+@icon("voxel_line.png")
 extends Node3D
 class_name VoxelLine
-#var material : Resource = preload("voxel.material")
+
+#var mesh_instance : PackedScene = preload("voxel.tscn")
+var material : Resource = preload("voxel.material")
+
 #@export var points = [
 #	Vector3(0, 0, 0),
 #	Vector3(0, 8, 16)
 #]
 #@export var width = 1.0
+
 var voxels : Array = []
 var pool : Array = []
 var pool_index : int = 0
@@ -18,18 +22,17 @@ var pool_index : int = 0
 #	if Engine.is_editor_hint():
 #		for i in range(0, points.size() - 1):
 #			var origin : Vector3 = get_global_transform().origin
-#			var A : Vector3 = points[i] + origin
-#			var B : Vector3 = points[i + 1] + origin
+#			var A  : Vector3 = points[i] + origin
+#			var B  : Vector3 = points[i + 1] + origin
 #			voxel_line(A, B, width)
 
 func add_voxel(p_size : float) -> MeshInstance3D:
 	var mesh_instance : MeshInstance3D = MeshInstance3D.new()
 	mesh_instance.set_name("Voxel_" + str(pool_index))
-	var mesh : BoxMesh = BoxMesh.new()
-	mesh.set_size(Vector3(p_size, p_size, p_size))
-	mesh_instance.set_mesh(mesh)
-#	mesh.material = material
-#	mesh.material.flags_unshaded = true
+	var box_mesh : BoxMesh = BoxMesh.new()
+	box_mesh.set_size(Vector3(p_size, p_size, p_size))
+	mesh_instance.set_mesh(box_mesh)
+	box_mesh.set_material(material)
 	return mesh_instance
 
 func get_pool(p_size : float = 1.0) -> MeshInstance3D:
@@ -38,6 +41,8 @@ func get_pool(p_size : float = 1.0) -> MeshInstance3D:
 		voxel = pool.pop_front()
 		voxel.set_visible(true)
 	else:
+#		voxel = mesh_instance.instantiate()
+#		voxel.get_mesh().set_size(Vector3(p_size, p_size, p_size))
 		voxel = add_voxel(p_size)
 		add_child(voxel)
 		pool_index += 1
@@ -49,10 +54,10 @@ func set_pool(p_voxel : MeshInstance3D) -> void:
 
 func voxel_line(p_start : Vector3, p_end : Vector3, p_size : float) -> void:
 	var direction : Vector3 = p_end - p_start
-	var length : float = direction.length();
+	var length : float = direction.length()
 	var inv_size : float = 1.0 / p_size
 	var step : int = ceili(length * inv_size) + 1
-#	var inv_steps : float = (1.0 / num_steps)
+#	var inv_step : float = (1.0 / step)
 	var distance : float = length / step
 	var offset : Vector3 = direction.normalized() * distance
 	if (step <= 0):
@@ -67,4 +72,7 @@ func voxel_line(p_start : Vector3, p_end : Vector3, p_size : float) -> void:
 			voxels.push_back(get_pool(p_size))
 	for i in range(step):
 		var voxel_origin : Vector3 = p_start + i * offset
+#		var voxel_origin : Vector3 = p_start + direction * (i * inv_step)
+#		var t : float = i / (step - 1.0)
+#		var voxel_origin : Vector3 = p_start + t * direction
 		voxels[i].global_transform.origin = round(voxel_origin * inv_size) * p_size
