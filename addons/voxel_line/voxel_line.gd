242b86a72c2b01b5ce6bc1081e0c52cb2dfb2f34
Nala Nedwob <nedwob@live.com>
2023-04-10 17:44:22 -0300

    Add files via upload

--- addons/voxel_line/voxel_line.gd
+++ addons/voxel_line/voxel_line.gd
@@ -1,37 +1,63 @@
 @tool
 @icon("voxel_line.png")
-extends Node3D
 class_name VoxelLine
+extends Node3D
 
 #var mesh_instance : PackedScene = preload("voxel.tscn")
-var material : Resource = preload("voxel.material")
-
-#@export var points : PackedVector3Array = [
-#	Vector3(0, 0, 0),
-#	Vector3(0, 8, 16) 
-#]
-#@export var width : float = 1.0
-#@export_color_no_alpha var color : Color = Color.CYAN
+var material : Resource = preload("voxel.material") as Material
 
 var voxels : Array = []
 var pool : Array = []
 var pool_index : int = 0
 
-#func _process(p_delta : float) -> void:
-#	if points.size() < 2:
-#		return
-#	if Engine.is_editor_hint():
-#		for i in range(0, points.size() - 1):
-#			var origin : Vector3 = get_global_transform().origin
-#			var A  : Vector3 = points[i] + origin
-#			var B  : Vector3 = points[i + 1] + origin
-#			voxel_line(A, B, width, color)
+@export var update : bool = false:
+	set(p_value):
+		update = p_value
+		_update_inspector()
+	get:
+		return update
+func _update_inspector():
+	notify_property_list_changed()
+
+@export var points : PackedVector3Array = [
+	Vector3(0, 0, 0),
+	Vector3(0, 8, 16) 
+]
+
+@export_color_no_alpha var color : Color = Color.CYAN:
+	set(p_value):
+		color = p_value
+		_set_color(color)
+	get:
+		return color
+
+@export_range(0.1, 16.0, 0.1) var width : float = 1.0:
+	set(p_value):
+		width = p_value
+	get:
+		return width
+
+func _draw3d() -> void:
+	if points.size() < 2:
+		return
+	if Engine.is_editor_hint():
+		var global_origin : Vector3 = get_global_transform().origin
+		for i in range(0, points.size() - 1):
+			var A : Vector3 = points[i] + global_origin
+			var B : Vector3 = points[i + 1] + global_origin
+			voxel_line(A, B, width, color)
+
+		_set_width(width)
+
+func _physics_process(p_delta : float) -> void:
+	_draw3d()
 
 func add_voxel(p_size : float, p_color : Color) -> MeshInstance3D:
 	var mesh_instance : MeshInstance3D = MeshInstance3D.new()
 	mesh_instance.set_name("Voxel_" + str(pool_index))
 	var box_mesh : BoxMesh = BoxMesh.new()
-	box_mesh.set_size(Vector3(p_size, p_size, p_size))
+#	box_mesh.set_size(Vector3(p_size, p_size, p_size))
+	box_mesh.size = Vector3.ONE * p_size
 	mesh_instance.set_mesh(box_mesh)
 	material.set_albedo(p_color)
 	box_mesh.set_material(material)
@@ -59,11 +85,11 @@ func voxel_line(p_start : Vector3, p_end : Vector3, p_size : float, p_color : Co
 	var length : float = direction.length()
 	var inv_size : float = 1.0 / p_size
 	var step : int = ceili(length * inv_size) + 1
-#	var inv_step : float = (1.0 / step)
-	var distance : float = length / step
-	var offset : Vector3 = direction.normalized() * distance
 	if (step <= 0):
 		return
+	var inv_step : float = 1.0 / step
+	var distance : float = length * inv_step # / step
+	var offset : Vector3 = direction.normalized() * distance
 	var size : int = voxels.size()
 	if (step < size):
 		for i in range(step, size):
@@ -78,3 +104,12 @@ func voxel_line(p_start : Vector3, p_end : Vector3, p_size : float, p_color : Co
 #		var t : float = i / (step - 1.0)
 #		var voxel_origin : Vector3 = p_start + t * direction
 		voxels[i].global_transform.origin = round(voxel_origin * inv_size) * p_size
+ 
+func _set_color(p_color : Color) -> void:
+	material.set_albedo(color)
+
+func _set_width(p_width : float) -> void:
+	for i in voxels.size():
+		var voxel : MeshInstance3D = voxels[i]
+		var mesh : BoxMesh = voxel.get_mesh()
+		mesh.size = Vector3.ONE * p_width
