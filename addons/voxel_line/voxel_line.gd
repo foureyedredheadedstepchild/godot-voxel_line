5460bae0746c275e276770131a1324f263d134db
Nala Nedwob <nedwob@live.com>
2023-04-11 01:45:56 -0300

    Add files via upload

--- addons/voxel_line/voxel_line.gd
+++ addons/voxel_line/voxel_line.gd
@@ -3,12 +3,16 @@
 class_name VoxelLine
 extends Node3D
 
-#var mesh_instance : PackedScene = preload("voxel.tscn")
+var multimesh_instance : MultiMeshInstance3D
+
+#var voxel_mesh_instance : PackedScene = preload("voxel.tscn")
 var material : Resource = preload("voxel.material") as Material
 
 var voxels : Array = []
 var pool : Array = []
 var pool_index : int = 0
+
+var enable_multimesh : bool = true
 
 @export var points : PackedVector3Array = [
 	Vector3(0, 0, 0),
@@ -28,6 +32,10 @@ var pool_index : int = 0
 	get:
 		return width
 
+func _ready():
+	if (enable_multimesh):
+		add_multimesh(get_global_transform().origin, 0, width, color)
+
 func _draw3d() -> void:
 	if points.size() < 2:
 		return
@@ -40,18 +48,43 @@ func _draw3d() -> void:
 
 		_set_width(width)
 
+
 func _physics_process(p_delta : float) -> void:
 	_draw3d()
+
+func add_multimesh(p_origin : Vector3, p_instance_count : int, p_size : float, p_color : Color) -> void:
+	if multimesh_instance:
+		return
+	multimesh_instance = MultiMeshInstance3D.new()
+	multimesh_instance.name = "Voxel"
+	material.set_albedo(p_color)
+	multimesh_instance.set_material_override(material)
+	add_child(multimesh_instance)
+	var multi_mesh : MultiMesh = MultiMesh.new()
+	multi_mesh.set_transform_format(MultiMesh.TRANSFORM_3D)
+	multi_mesh.set_instance_count(p_instance_count)
+	multi_mesh.visible_instance_count = -1
+#	multi_mesh.use_colors = true
+#	multi_mesh.set_instance_color(0, Color(1.0, 1.0, 1.0, 1.0))
+	var voxel_mesh : BoxMesh = BoxMesh.new()
+#	voxel_mesh.set_size(Vector3(p_size, p_size, p_size))
+	voxel_mesh.size = Vector3.ONE * p_size
+	multi_mesh.mesh = voxel_mesh
+	multimesh_instance.multimesh = multi_mesh
+
+func get_multimesh_instance() -> MultiMeshInstance3D:
+	return multimesh_instance
+
 
 func add_voxel(p_size : float, p_color : Color) -> MeshInstance3D:
 	var mesh_instance : MeshInstance3D = MeshInstance3D.new()
 	mesh_instance.set_name("Voxel_" + str(pool_index))
-	var box_mesh : BoxMesh = BoxMesh.new()
-#	box_mesh.set_size(Vector3(p_size, p_size, p_size))
-	box_mesh.size = Vector3.ONE * p_size
-	mesh_instance.set_mesh(box_mesh)
+	var voxel_mesh : BoxMesh = BoxMesh.new()
+#	voxel_mesh.set_size(Vector3(p_size, p_size, p_size))
+	voxel_mesh.size = Vector3.ONE * p_size
+	mesh_instance.set_mesh(voxel_mesh)
 	material.set_albedo(p_color)
-	box_mesh.set_material(material)
+	voxel_mesh.set_material(material)
 	return mesh_instance
 
 func get_pool(p_size : float, p_color : Color) -> MeshInstance3D:
@@ -60,7 +93,7 @@ func get_pool(p_size : float, p_color : Color) -> MeshInstance3D:
 		voxel = pool.pop_front()
 		voxel.set_visible(true)
 	else:
-#		voxel = mesh_instance.instantiate()
+#		voxel = voxel_mesh_instance.instantiate()
 #		voxel.get_mesh().set_size(Vector3(p_size, p_size, p_size))
 		voxel = add_voxel(p_size, p_color)
 		add_child(voxel)
@@ -81,20 +114,34 @@ func voxel_line(p_start : Vector3, p_end : Vector3, p_size : float, p_color : Co
 	var inv_step : float = 1.0 / step
 	var distance : float = length * inv_step # / step
 	var offset : Vector3 = direction.normalized() * distance
-	var size : int = voxels.size()
-	if (step < size):
-		for i in range(step, size):
-			set_pool(voxels[i]) 
-		voxels.resize(step)
+	
+	if (enable_multimesh):
+		if (multimesh_instance):
+			var multimesh : MultiMesh = multimesh_instance.get_multimesh()
+			multimesh.set_instance_count(step)
+			multimesh.mesh.size = Vector3.ONE * p_size
+			var instance_count : int = multimesh.get_instance_count()
+			for i in range(instance_count):
+				var voxel_transform : Transform3D = Transform3D()
+				var voxel_origin : Vector3 = p_start + i * offset
+				voxel_transform.origin = to_local(round(voxel_origin * inv_size) * p_size)
+				multimesh.set_instance_transform(i, voxel_transform)
 	else:
-		for i in range(size, step):
-			voxels.push_back(get_pool(p_size, p_color))
-	for i in range(step):
-		var voxel_origin : Vector3 = p_start + i * offset
-#		var voxel_origin : Vector3 = p_start + direction * (i * inv_step)
-#		var t : float = i / (step - 1.0)
-#		var voxel_origin : Vector3 = p_start + t * direction
-		voxels[i].global_transform.origin = round(voxel_origin * inv_size) * p_size
+		var size : int = voxels.size()
+		if (step < size):
+			for i in range(step, size):
+				set_pool(voxels[i]) 
+			voxels.resize(step)
+		else:
+			for i in range(size, step):
+				voxels.push_back(get_pool(p_size, p_color))
+
+		for i in range(step):
+			var voxel_origin : Vector3 = p_start + i * offset
+	#		var voxel_origin : Vector3 = p_start + direction * (i * inv_step)
+	#		var t : float = i / (step - 1.0)
+	#		var voxel_origin : Vector3 = p_start + t * direction
+			voxels[i].global_transform.origin = round(voxel_origin * inv_size) * p_size
  
 func _set_color(p_color : Color) -> void:
 	material.set_albedo(color)
