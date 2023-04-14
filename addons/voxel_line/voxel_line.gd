39376a9b8355fa994dfc5592ed280392255ec6d7
Nala Nedwob <nedwob@live.com>
2023-04-13 21:19:07 -0300

    Add files via upload

--- addons/voxel_line/voxel_line.gd
+++ addons/voxel_line/voxel_line.gd
@@ -14,6 +14,7 @@ var material : Resource = preload("voxel.material") as Material
 	set(p_value):
 		points = p_value
 		_update()
+		notify_property_list_changed()
 	get:
 		return points
 
@@ -27,32 +28,33 @@ var material : Resource = preload("voxel.material") as Material
 @export_color_no_alpha var color : Color = Color.CYAN:
 	set(p_value):
 		color = p_value
-		_set_color(color)
+#		_set_color(color)
 		_update()
 	get:
 		return color
 
-func _ready() -> void:
+func _ready():
 	set_multi_mesh(0, size, color)
 	_update()
 
 func _update() -> void:
-#	if Engine.is_editor_hint():
 	if (is_inside_tree()):
 		if (points.size() < 2):
 			return
-		var global_origin : Vector3 = get_global_transform().origin
+		var origin : Vector3 = get_global_transform().origin
 		for i in range(0, points.size() - 1):
-			var start : Vector3 = points[i] + global_origin
-			var end : Vector3 = points[i + 1] + global_origin
+			var start : Vector3 = points[i] + origin
+			var end : Vector3 = points[i + 1] + origin
 			voxel_line(start, end, size, color)
 
 func set_multi_mesh(p_instance_count : int, p_size : float, p_color : Color) -> void:
-	if (multi_mesh):
+	if (multi_mesh != null):
 		return
 	multi_mesh = MultiMesh.new()
 	multi_mesh.set_transform_format(MultiMesh.TRANSFORM_3D)
 	multi_mesh.set_instance_count(p_instance_count)
+#	multi_mesh.set_use_colors(true)
+#	multi_mesh.set_instance_color(p_instance_count, p_color)
 	var mesh : BoxMesh = BoxMesh.new()
 	mesh.set_size(Vector3.ONE * p_size)
 	mesh.set_material(material)
@@ -64,22 +66,22 @@ func voxel_line(p_start : Vector3, p_end : Vector3, p_size : float, p_color : Co
 	var length : float = direction.length()
 	var inv_size : float = 1.0 / p_size
 	var step : int = ceili(length * inv_size)
-	if (step <= 0):
+	if (step <= 0): 
 		return
-	var inv_step : float = 1.0 / step
+	var inv_step : float = 1.0 / float(step)
 	var distance : float = length * inv_step
 	var offset : Vector3 = direction.normalized() * distance
-	if (multi_mesh):
+	if (multi_mesh != null):
 		multi_mesh.set_instance_count(step)
 		multi_mesh.get_mesh().set_size(Vector3.ONE * p_size)
 		var instance_count : int = multi_mesh.get_instance_count()
 		for i in range(instance_count):
-			var voxel_transform : Transform3D
-			var voxel_origin : Vector3 = p_start + i * offset
-			voxel_transform.origin = to_local(round(voxel_origin * inv_size) * p_size)
+			var voxel_transform : Transform3D = Transform3D()
+			var origin : Vector3 = (p_start - get_global_transform().origin) + i * offset
+			voxel_transform.origin = round(origin * inv_size) * p_size
 			multi_mesh.set_instance_transform(i, voxel_transform)
 
 func _set_color(p_color : Color) -> void:
-	if (multi_mesh):
+	if (multi_mesh != null):
 		var mesh : BoxMesh = multi_mesh.get_mesh()
 		mesh.get_material().set_albedo(p_color)
